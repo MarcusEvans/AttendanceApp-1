90103c7267daa8c73965c7bef47bf7a181f993c4
MarcusEvans <marcusevans03@gmail.com>
2018-01-22 10:52:27 -0600

    New Array

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,13 @@ public class Main {
 
         System.out.println("\nHello " + name + "\n");
 
-        // Create and output List of absences
+        //TODO Create and output List of absences
 
         ArrayList<Integer> numbers = new ArrayList<>();
 
         Random rand = new Random();
 
-        System.out.println("How many Absences would you like to output " + name +"\n");
+        System.out.println("How many Absences would you like to output " + name + "\n");
 
         int num = sc.nextInt();
         for (int i = 0; i < num; i++) {
@@ -30,14 +30,31 @@ public class Main {
         }
         System.out.println(numbers);
 
-        //How many of the absences did people have perfect attendance
+        //TODO How many of the absences did people have perfect attendance
 
         int perfectCount = 0;
         for (int i = 0; i < num; i++) {
-            if (numbers.get(i) == 0){
-                perfectCount ++;
+            if (numbers.get(i) == 0) {
+                perfectCount++;
             }
         }
         System.out.println("\nThe number of perfect Attendances are: " + perfectCount);
+
+        //TODO What is the average of the attendances
+
+        ArrayList avg = average(numbers,num);
+        System.out.println("The average is " + avg);
     }
+
+    public static ArrayList<Integer> average(ArrayList<Integer> answer, int storedNumOfVal) {
+        int sum = 0;
+        double avg = 0;
+        for (int i = 0; i < storedNumOfVal; i++) {
+            sum = answer.get(i) + sum;
+            avg = sum / answer.size();
+        }
+        return answer;
+    }
+
 }
+
