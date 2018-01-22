7e883e69eff01d9fb0b77e0757b7e0a316f8f826
MarcusEvans <marcusevans03@gmail.com>
2018-01-22 10:18:22 -0600

    Write Welcome Message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHi, AttendanceApp!\n");
+
+        System.out.println("What is your name??\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+
+        System.out.println("\nHello " + name + "\n");
     }
 }
